9caed8a7a27047636c5fc9f64f340c8c277e11b4
Mr_GreZevS <danil_grezev@mail.ru>
2019-06-14 20:30:00 +0700

    Ну шо, поiхалi?

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject settings;
+
+	public void NewGame()
+    {
+        Application.LoadLevel(1);       
+    }
+
+    public void ContineGame()
+    {
+
+    }
+
+    public void Settings()
+    {
+        settings.SetActive(!settings.activeSelf);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();   
+    }
+}
