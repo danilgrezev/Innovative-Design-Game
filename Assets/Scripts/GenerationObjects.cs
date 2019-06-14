741a979f30934dc4a063b89aa3e73bdbe16ff0be
DanilBabenkov <babenkovdan1@gmail.com>
2019-06-15 01:27:45 +0700

    добавил генерацию предметов на карте

--- Assets/Scripts/GenerationObjects.cs
+++ Assets/Scripts/GenerationObjects.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class GenerationObjects : MonoBehaviour
 {
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(EnemyDrop());
+    }
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(5, 115);
+            zPos = Random.Range(5, 108);
+            Instantiate(theEnemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+
+
+        }
     }
 
     // Update is called once per frame
